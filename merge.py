b2eaae7cf9166e0edfb33d1ebb2e127d4d9af5dc
kami <kamilove1998@gmail.com>
2021-10-27 01:54:58 +0900

    index 초기화 추가

--- merge.py
+++ merge.py
@@ -8,4 +8,5 @@ from collections import defaultdict
 df = pd.read_csv("guild_member.csv")
 df = df[['닉네임', '직업', '레벨', '직위']]
 df.sort_values(by=['닉네임'], axis=0, inplace=True)
+df.reset_index(inplace=True, drop=True)
 df.to_csv("sorted_guild_member.csv", encoding="utf-8-sig")
